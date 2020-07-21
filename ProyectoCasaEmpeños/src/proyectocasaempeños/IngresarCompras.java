1f4edc60cfe5decc848d62c9955be0e50e3b2a03
Luis Oseguera <osegueraluis436@gmail.com>
2020-07-21 14:53:59 -0600

    Adición del formulario para buscar clientes

--- ProyectoCasaEmpeños/src/proyectocasaempeños/IngresarCompras.java
+++ ProyectoCasaEmpeños/src/proyectocasaempeños/IngresarCompras.java
@@ -47,12 +47,13 @@ public class IngresarCompras extends javax.swing.JFrame {
         btnAgregarProducto1 = new javax.swing.JButton();
         btnEliminarProducto = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tblComprasIngresar = new javax.swing.JTable();
         txtTotal = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         btnIngresarCompra = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
         txtFecha = new javax.swing.JTextField();
+        btnRegresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -137,18 +138,35 @@ public class IngresarCompras extends javax.swing.JFrame {
 
         btnEliminarProducto.setText("Eliminar producto");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tblComprasIngresar.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nombre", "Cantidad", "Precio"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblComprasIngresar);
+        if (tblComprasIngresar.getColumnModel().getColumnCount() > 0) {
+            tblComprasIngresar.getColumnModel().getColumn(0).setResizable(false);
+            tblComprasIngresar.getColumnModel().getColumn(1).setResizable(false);
+            tblComprasIngresar.getColumnModel().getColumn(2).setResizable(false);
+        }
 
         txtTotal.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -223,6 +241,8 @@ public class IngresarCompras extends javax.swing.JFrame {
 
         txtFecha.setEditable(false);
 
+        btnRegresar.setText("Regresar");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -244,7 +264,9 @@ public class IngresarCompras extends javax.swing.JFrame {
                             .addContainerGap()
                             .addComponent(grpDatosCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(230, 230, 230)
+                        .addContainerGap()
+                        .addComponent(btnRegresar)
+                        .addGap(147, 147, 147)
                         .addComponent(btnIngresarCompra)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -252,16 +274,19 @@ public class IngresarCompras extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtFecha, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)
+                        .addComponent(jLabel9)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(grpDatosCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(grpDetallesCompra, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnIngresarCompra)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnIngresarCompra)
+                    .addComponent(btnRegresar))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -340,6 +365,7 @@ public class IngresarCompras extends javax.swing.JFrame {
     private javax.swing.JButton btnAgregarProducto1;
     private javax.swing.JButton btnEliminarProducto;
     private javax.swing.JButton btnIngresarCompra;
+    private javax.swing.JButton btnRegresar;
     private javax.swing.JButton btnSeleccionarCliente;
     private javax.swing.JPanel grpDatosCliente;
     private javax.swing.JPanel grpDetallesCompra;
@@ -354,7 +380,7 @@ public class IngresarCompras extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable tblComprasIngresar;
     private javax.swing.JTextField txtApellidoCliente;
     private javax.swing.JTextField txtCantidadProducto;
     private javax.swing.JTextField txtFecha;
